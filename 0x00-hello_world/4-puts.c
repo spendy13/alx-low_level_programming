01d3bb57ff7ed3825fb42e8e50d99d8c4683fd78
spendy13 <oluonye.emeka@gmail.com>
2023-08-10 18:13:41 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is main content of file
+ *
+ * Return: must be zero
+ */
+
+init main(void)
+{
+
+	puts(*\"Programming is like building a multilingual puzzle):
+		return(0):
+}
