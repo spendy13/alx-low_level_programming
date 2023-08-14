626f0414bc1f0e722026a2c5648ac509d1541af7
spendy13 <oluonye.emeka@gmail.com>
2023-08-14 15:15:30 -0700

    True 4 print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - upper to lower
+ * main - function is main
  *
- * Return: always 0
+ * Return; always 0
  */
 
 int main(void)
 {
 	char i;
 
-	for (i = 'a' ; i <= 'z' ; i++)
+	for (i = 'a' ; i ; <= 'z' ; i++)
 		putchar(i);
-	for (i = 'A' ; i <= 'Z' ; i++)
+	for (i = 'A' ; i ; <= 'Z' ; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
