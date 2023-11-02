565d90a318ae681893c834ad54367fafea131eee
spendy13 <oluonye.emeka@gmail.com>
2023-11-02 11:05:56 -0700

    isalpha

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,8 +17,9 @@ int print_sign(int n)
 	else if (n == 0)
 	{
 		_putchar(48);
-	}	return (0);
-	 if (n < 0)
+	}
+	return (0);
+	if (n < 0)
 	{
 		_putchar('-');
 	}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - functions that computes the absolute value of an int.
+ * @r: the value
+ * Description: the standard library provides a similar function
+ * Return: (0)
+ */
+int _abs(int r)
+{
+if (r >= 0)
+return (r);
+else
+return (r * -1);
+}
