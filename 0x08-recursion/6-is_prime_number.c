7976bf69b89c92995f3337ecea53ffffd112621b
spendy13 <oluonye.emeka@gmail.com>
2023-11-02 09:07:13 -0700

    wildcamp

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * prime_a - returns an integer if it is a prime number
+ * @a: input
+ * @b: divisor
+ * Return: (0)
+ */
+int prime_a(int a, int b)
+{
+        if (a <= 1 || (a != b && a % b == 0))
+        {
+                return (0);
+        }
+        else if (a == b)
+        {
+                return (1);
+        }
+        return (prime_a(a, b + 1));
+}
+
+/**
+ * is_prime_number - detect if its a prime number
+ * @n: the input number
+ * Return: 0 or 1
+ */
+int is_prime_number(int n)
+{
+        return (prime_a(n, 2));
+}
