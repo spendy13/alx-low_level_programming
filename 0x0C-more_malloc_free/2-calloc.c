9e122f73c6cb85562453c62d0219b660b654d9de
spendy13 <oluonye.emeka@gmail.com>
2023-11-02 10:19:19 -0700

    concat

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,13 +10,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i;
+	unsigned int i;
 
-        for (i = 0; i < n; i++)
-        {
-                s[i] = b;
-        }
-        return (s);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
 /**
  * *_calloc - allocates memory for an array
@@ -27,13 +27,13 @@ char *_memset(char *s, char b, unsigned int n)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-        char *ptr;
+	char *ptr;
 
-        if (nmemb == 0 || size == 0)
-                return (NULL);
-        ptr = malloc(size * nmemb);
-        if (ptr == NULL)
-                return (NULL);
-        _memset(ptr, 0, nmemb * size);
-        return (ptr);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
+	return (ptr);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,16 +9,16 @@
  */
 int *array_range(int min, int max)
 {
-        int *ptr;
-        int i, size;
+	int *ptr;
+	int i, size;
 
-        if (min > max)
-                return (NULL);
-        size = max - min + 1;
-        ptr = malloc(sizeof(int) * size);
-        if (ptr == NULL)
-                return (NULL);
-        for (i = 0; min <= max; i++)
-                ptr[i] = min++;
-        return (ptr);
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+	ptr = malloc(sizeof(int) * size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
+	return (ptr);
 }
